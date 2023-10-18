47fe6cb8b008104d8a24a10150f5f21f72a3d273
unknown <176493@BME.HU>
2023-10-18 16:07:31 +0200

    Add - tests

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -17,4 +17,32 @@ class TorpedoStoreTest {
     // Assert
     assertEquals(true, result);
   }
+
+  @Test
+  void fire_Failrule_with_count(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(3, 1);
+
+    // Act
+    boolean result = store.fire(1);
+    int countResult = store.getTorpedoCount();
+    //Assert
+    assertEquals(false, result);
+    assertEquals(3, countResult);
+  }
+
+  @Test
+  void fire_Count_and_not_empty(){
+    // Arrange
+    TorpedoStore store = new TorpedoStore(3, 0);
+
+    // Act
+    store.fire(1);
+    int result = store.getTorpedoCount();
+    boolean empty = store.isEmpty();
+
+    //Assert
+    assertEquals(2, result);
+    assertEquals(false, empty);
+  }
 }
